a579c47bbd3f36e0e7f513da21e931ca102b48f6
yenxill <dri1356@gamil.com>
2024-06-08 23:13:59 +0900

    ✨ 수업

--- 태그연습/input/수업(240607)/script.js
+++ 태그연습/input/수업(240607)/script.js
@@ -11,21 +11,21 @@ console.log(getMonth)
 
 
 for(let i = 2000; i <= getYear; i++) {
-  //console.log(i)
-  //let options = document.createElement('option');
-  //console.log(options)
-  //options.innerHTML = i;
-  //year.appendChild(options);
+  // console.log(i)
+  // let options = document.createElement('option');
+  // console.log(options)
+  // options.innerHTML = i;
+  // year.appendChild(options);
 
   let options = `<option>${i}</option>`;
-  //console.log(options)
+  // //console.log(options)
   
-  //afterbegin, beforebegin, afterend, beforend
-  // 추가하고 싶은 곳.insertAdjacentHTML('위치', 추가할 요소)
-  year.insertAdjacentHTML('beforeend', options); 
+  // //afterbegin, beforebegin, afterend, beforend
+  // // 추가하고 싶은 곳.insertAdjacentHTML('위치', 추가할 요소)
+  // year.insertAdjacentHTML('beforeend', options); 
 
-  //jQuery
-  $('.year').append('<option>' + i + '</option>');
+  // //jQuery
+  // $('.year').append('<option>' + i + '</option>');
 
 
 }
